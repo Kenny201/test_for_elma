442c5779351065f9789dd8a8873029153a23c0f9
Vitaiy Izmestjev <panins899@gmail.com>
2022-02-21 16:47:59 +0300

    feature:moved logic from handler to service

--- app/Console/Commands/FindCryptoPair.php
+++ app/Console/Commands/FindCryptoPair.php
@@ -2,10 +2,9 @@
 
 namespace App\Console\Commands;
 
-use App\Http\Integrations\Blockchain\BlockchainConnector;
-use App\Http\Integrations\Coingecko\CoingeckoConnector;
+use App\Services\Crypto\BlockchainService;
+use App\Services\Crypto\CoingeckoService;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 
 class FindCryptoPair extends Command
@@ -40,32 +39,22 @@ class FindCryptoPair extends Command
      *
      * @return int
      */
-    public function handle()
+    public function handle(): int
     {
         $crypto_name = Str::upper($this->ask('Enter the name of the cryptocurrency?'));
         $currency = Str::upper($this->ask('Enter the name of the currency to be exchanged?'));
-        $symbol = $crypto_name . '-' . $currency;
-        $coins = [];
-        $info_text = '';
 
         $requests = [
-            'coingecko' => CoingeckoConnector::getCoingeckoPriceServerRequest($crypto_name, $currency),
-            'blockchain' => BlockchainConnector::getBlockchainServerRequest($symbol),
+            CoingeckoService::find($crypto_name, $currency),
+            BlockchainService::find($crypto_name, $currency),
         ];
 
-        foreach ($requests as $k => $request) {
-            $coingecko_name = $request->crypto_name ?? '';
-            $currency_lower = Str::lower($currency);
-
-            $response = $request->send();
-            $response = $response->json();
-
-            $coins[$k]['url'] = Str::of(parse_url($request->getFullRequestUrl(), PHP_URL_HOST))->after('api.');
-            $coins[$k]['price'] = $response[$coingecko_name][$currency_lower] ?? $response['last_trade_price'] ?? 'No such pair found.';
-            $this->info('A source: ' . $coins[$k]['url']);
-            $this->info( 'Rate: ' . $coins[$k]['price']);
+        foreach ($requests as $request) {
+            $this->info('A source: ' . $request['url']);
+            $this->info('Rate: ' . $request['price']);
             $this->newLine();
         }
+
         return 0;
     }
 }
